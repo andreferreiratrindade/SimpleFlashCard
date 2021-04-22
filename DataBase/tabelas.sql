98ac94348f2b832cc9e75ee57a1ec0c9468c2811
Andre <andreferreiratrindade@gmail.com>
2021-04-22 13:05:06 -0300

    Criando tabelas cartao

--- DataBase/tabelas.sql
+++ DataBase/tabelas.sql
@@ -8,4 +8,46 @@ create table if not exists Pessoa(
     TxtSenha varchar(500) not null,
     TxtEmail varchar(500) not null,
     DtaIncusao TIMESTAMP  default current_timestamp
-)
+);
+
+create table if not exists Conteudo(
+	IdConteudo	int auto_increment primary key,
+	IdPessoa int not null, 
+    NmeConteudo varchar(500) not null,
+    DtaIncusao TIMESTAMP  default current_timestamp,
+
+    FOREIGN KEY (IdPessoa)
+        REFERENCES Pessoa (IdPessoa)
+        ON UPDATE RESTRICT ON DELETE CASCADE
+);
+
+create table if not exists Cartao(
+    IdCartao int auto_increment primary key, 
+    IdConteudo int not null, 
+    DtaIncusao TIMESTAMP  default current_timestamp,
+
+    FOREIGN KEY (IdConteudo)
+    REFERENCES Conteudo (IdConteudo)
+    ON UPDATE RESTRICT ON DELETE CASCADE
+);
+
+create table if not exists CartaoPergunta(
+    IdCartaoPergunta int auto_increment primary key, 
+    IdCartao int not null, 
+    TxtPergunta  Text  not null, 
+    DtaIncusao TIMESTAMP  default current_timestamp,
+
+    FOREIGN KEY (IdCartao)
+    REFERENCES Cartao (IdCartao)
+    ON UPDATE RESTRICT ON DELETE CASCADE
+);
+
+create table if not exists CartaoResposta(
+    IdCartaoResposta int auto_increment primary key, 
+    IdCartao int not null, 
+    TxtResposta  Text  not null, 
+    DtaIncusao TIMESTAMP  default current_timestamp,
+    FOREIGN KEY (IdCartao)
+    REFERENCES Cartao (IdCartao)
+    ON UPDATE RESTRICT ON DELETE CASCADE
+);
