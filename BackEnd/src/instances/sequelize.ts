f159e9382cc99b1467ee9fe13550125b26c6297e
Andre <andreferreiratrindade@gmail.com>
2021-04-25 00:47:27 -0300

    Adicionando banco de dados remoto

--- BackEnd/src/instances/sequelize.ts
+++ BackEnd/src/instances/sequelize.ts
@@ -4,14 +4,15 @@ import { Conteudo } from '../models/conteudoModel';
 import { Pergunta } from '../models/perguntaModel';
 import {  Pessoa } from '../models/pessoaModel';
 import { Resposta } from '../models/respostaModel';
-
-const db = 'SimpleFlashCard'
-const username = 'root'
+//https://freedb.tech/dashboard/index.php
+const db = 'freedbtech_SimpleFlashCard'
+const username = 'freedbtech_ifpt'
 const password = 'Mudar123'
 
 export const sequelize = new Sequelize(db, username, password, {
   dialect: "mysql",
   port: 3306,
+  host:"freedb.tech",
   models: [Pessoa, Conteudo, Cartao, Pergunta, Resposta],
   repositoryMode: true,
 });
