3c6dd0ab2a4ee3c1fee95c64d59244bd8e4f4762
Andre <andreferreiratrindade@gmail.com>
2021-05-01 14:48:50 -0300

    Adicionando Banco Web

--- BackEnd/src/instances/sequelize.ts
+++ BackEnd/src/instances/sequelize.ts
@@ -1,21 +1,19 @@
 import { Sequelize } from 'sequelize-typescript';
-import { Avaliacao } from '../models/avaliacaoModel';
 import { Cartao } from '../models/cartaoModel';
 import { Conteudo } from '../models/conteudoModel';
 import { Pergunta } from '../models/perguntaModel';
 import {  Pessoa } from '../models/pessoaModel';
 import { Resposta } from '../models/respostaModel';
 //https://freedb.tech/dashboard/index.php
-// const db = 'freedbtech_SimpleFlashCard'
-// const username = 'freedbtech_ifpt'
-// const password = 'Mudar123'
-const db = 'SimpleFlashCard'
-const username = 'root'
+const db = 'freedbtech_SimpleFlashCard'
+const username = 'freedbtech_ifpt'
 const password = 'Mudar123'
+
 export const sequelize = new Sequelize(db, username, password, {
   dialect: "mysql",
   port: 3306,
-  models: [Pessoa, Conteudo, Cartao, Pergunta, Resposta, Avaliacao],
+  host:"freedb.tech",
+  models: [Pessoa, Conteudo, Cartao, Pergunta, Resposta],
   repositoryMode: true,
 });
 
